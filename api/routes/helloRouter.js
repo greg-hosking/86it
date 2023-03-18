932b95000fa3d7650880ed2ab956708d4a0243d9
Greg Hosking <hoskinggregory@gmail.com>
2023-03-18 15:56:05 -0400

    Moved and renamed files

--- api/routes/helloRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const helloRouter = express.Router();
-
-helloRouter.get('/', function (req, res, next) {
-  res.json({ message: 'Hello, World!' });
-});
-
-export default helloRouter;
