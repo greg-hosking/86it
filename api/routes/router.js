838f11f38e3158fb0e5a341da68a837ce97c79e1
Greg Hosking <hoskinggregory@gmail.com>
2023-04-10 21:18:49 -0400

    Add auth router for email verification and sign in

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,10 +1,12 @@
 import helloRouter from './hello.js';
 import helloDBRouter from './helloDB.js';
 import usersRouter from './users.js';
+import authRouter from './auth.js';
 
 export default function (app) {
   const prefix = '/api';
   app.use(`${prefix}/hello`, helloRouter);
   app.use(`${prefix}/helloDB`, helloDBRouter);
   app.use(`${prefix}/users`, usersRouter);
+  app.use(`${prefix}/auth`, authRouter);
 }
