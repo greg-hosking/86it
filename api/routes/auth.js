3231b48362c98b9e56df19bbd050ea170acadda2
Greg Hosking <hoskinggregory@gmail.com>
2023-04-11 00:58:27 -0400

    Began implementing password recovery

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -7,8 +7,17 @@ import withDB from '../utils/db.js';
 import bcrypt from 'bcrypt';
 import createError from 'http-errors';
 import jwt from 'jsonwebtoken';
+import nodemailer from 'nodemailer';
 
 const router = express.Router();
+
+const transporter = nodemailer.createTransport({
+  host: config.gmail.host,
+  auth: {
+    user: config.gmail.user,
+    pass: config.gmail.pass,
+  },
+});
 
 /**
  *
@@ -17,7 +26,7 @@ router.get('/verification', async function (req, res, next) {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, config.gmail.secret, async function (err, user) {
+    jwt.verify(token, config.gmail.secret, async function (err, decoded) {
       if (err) {
         return next(createError(403));
       }
@@ -26,7 +35,7 @@ router.get('/verification', async function (req, res, next) {
         const users = db.collection('users');
         const ObjectId = mongodb.ObjectId;
         return await users.updateOne(
-          { _id: new ObjectId(user.id) },
+          { _id: new ObjectId(decoded.id) },
           { $set: { emailVerified: true } }
         );
       }, res);
@@ -34,9 +43,9 @@ router.get('/verification', async function (req, res, next) {
         return next(createError(500));
       }
 
-      return res
-        .status(200)
-        .json({ message: `Successfully verified email for user ${user.id}` });
+      return res.status(200).json({
+        message: `Successfully verified email for user ${decoded.id}`,
+      });
     });
   } else {
     return next(createError(401));
@@ -59,7 +68,6 @@ router.post('/', async function (req, res, next) {
       // TODO: Add error description
       return next(createError(404));
     }
-
     if (!user.emailVerified) {
       // TODO: Add error description
       return next(createError(403));
@@ -73,7 +81,7 @@ router.post('/', async function (req, res, next) {
 
     jwt.sign(
       {
-        id: user.id,
+        id: user._id,
       },
       config.gmail.secret,
       {
@@ -92,6 +100,50 @@ router.post('/', async function (req, res, next) {
       }
     );
   }, res);
+});
+
+/**
+ *
+ */
+router.post('/recovery', async function (req, res, next) {
+  const email = req.body.email;
+  if (!email) {
+    return next(createError(400, 'Request body is missing email.'));
+  }
+
+  await withDB(async function (db) {
+    const users = db.collection('users');
+    const user = users.findOne({ email: email });
+    if (!user) {
+      return next(createError(404));
+    }
+
+    // Send recovery email asynchronously.
+    jwt.sign(
+      {
+        id: user._id,
+      },
+      config.gmail.secret,
+      {
+        expiresIn: '1d',
+      },
+      function (err, token) {
+        if (err) {
+          return next(createError(500, err));
+        }
+        const url = `http://localhost:5173/temp/reset-password?token=${token}`;
+
+        transporter.sendMail({
+          to: email,
+          subject: 'Recover your 86it password',
+          html: `Please <a href="${url}">click here</a> to recover your password. 
+                 If you didn't request this, please ignore this email.`,
+        });
+      }
+    );
+  }, res);
+
+  res.status(200).json({ message: 'Recovery email sent.' });
 });
 
 export default router;
