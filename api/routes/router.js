ffe6a79b39804d8da61cde6d3e064187c0f7eef7
Greg Hosking <hoskinggregory@gmail.com>
2023-04-11 02:18:56 -0400

    Remove hello endpoints

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,12 +1,9 @@
-import helloRouter from './hello.js';
-import helloDBRouter from './helloDB.js';
 import usersRouter from './users.js';
 import authRouter from './auth.js';
 
 export default function (app) {
   const prefix = '/api';
-  app.use(`${prefix}/hello`, helloRouter);
-  app.use(`${prefix}/helloDB`, helloDBRouter);
+
   app.use(`${prefix}/users`, usersRouter);
   app.use(`${prefix}/auth`, authRouter);
 }
