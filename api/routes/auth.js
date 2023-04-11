561c9ccaa1501b80e36d02f66f5cab50837d39fb
Greg Hosking <hoskinggregory@gmail.com>
2023-04-10 21:18:21 -0400

    Add auth router for email verification and sign in

---
+++ api/routes/auth.js
@@ -0,0 +1,90 @@
+import express from 'express';
+
+import config from '../utils/env.js';
+import withDB from '../utils/db.js';
+
+import bcrypt from 'bcrypt';
+import createError from 'http-errors';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+/**
+ *
+ */
+router.get('/verification', async function (req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, config.gmail.secret, async function (err, user) {
+      if (err) {
+        return next(createError(403));
+      }
+      // Once token verified, update emailVerified in user.
+      const result = await withDB(async function (db) {
+        const users = db.collection('users');
+        return await users.updateOne(
+          { _id: user.id },
+          { $set: { emailVerified: true } }
+        );
+      }, res);
+      if (!result) {
+        return next(createError(500));
+      }
+
+      return res
+        .status(200)
+        .json({ message: `Successfully verified email for user ${user.id}` });
+    });
+  } else {
+    return next(createError(401));
+  }
+});
+
+/**
+ *
+ */
+router.post('/', async function (req, res, next) {
+  const { email, password } = req.body;
+  if (!(email && password)) {
+    return next(createError(400, 'Request body is missing email or password.'));
+  }
+
+  await withDB(async function (db) {
+    const users = db.collection('users');
+    const user = await users.findOne({ email: email });
+    if (!user) {
+      // TODO: Add error description
+      return next(createError(404));
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      // TODO: Add error description
+      return next(createError(401));
+    }
+
+    jwt.sign(
+      {
+        id: user.id,
+      },
+      config.gmail.secret,
+      {
+        expiresIn: '1d',
+      },
+      function (err, token) {
+        if (err) {
+          return next(createError(500, err));
+        }
+
+        return res
+          .status(200)
+          .clearCookie('86itCookie')
+          .cookie('86itCookie', token, { httpOnly: true })
+          .send();
+      }
+    );
+  }, res);
+});
+
+export default router;
