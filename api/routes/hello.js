2a7dcf24cabb76353269b46a278b1226622674e1
Greg Hosking <hoskinggregory@gmail.com>
2023-04-11 02:18:17 -0400

    Remove hello endpoints

--- api/routes/hello.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-  res.json({ message: 'Hello from 86it!' });
-});
-
-export default router;
