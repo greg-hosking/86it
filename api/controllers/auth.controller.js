f2317ffa8f508b01ec50a583915246e5d498ca99
Greg Hosking <hoskinggregory@gmail.com>
2023-04-13 00:43:44 -0400

    Fix handler function exports in controllers

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,133 +6,141 @@ import transporter from '../utils/mail.js';
 import createError from 'http-errors';
 import jwt from 'jsonwebtoken';
 
-export async function verifyEmail(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!(authHeader && authHeader.startsWith('Bearer '))) {
-    return next(createError(401, 'Request headers are missing authorization.'));
-  }
-
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, config.gmail.secret, async function (err, decoded) {
-    if (err) {
-      return next(createError(403, 'Failed to verify token.'));
-    }
-    const user = await User.findOne({ _id: decoded.id }).exec();
-    if (!user) {
-      return next(createError(404, 'User not found.'));
-    }
-    user.emailVerified = true;
-    await user.save();
-  });
-
-  return res.status(200).json('Email verified.');
-}
-
-export async function sendResetPasswordEmail(req, res, next) {
-  const { email } = req.body;
-  if (!email) {
-    return next(createError(400, 'Request body is missing email.'));
-  }
-
-  const user = await User.findOne({ email: email }).exec();
-  if (!user) {
-    return next(createError(404, 'User not found.'));
-  }
-  if (!user.emailVerified) {
-    return next(createError(403, 'Email not verified.'));
-  }
-
-  jwt.sign(
-    { id: user._id },
-    config.gmail.secret,
-    { expiresIn: '1d' },
-    function (err, token) {
-      if (err) {
-        return next(createError(500, err));
-      }
-      const url = `http://localhost:5173/temp/reset-password?token=${token}`;
-      transporter.sendMail(
-        {
-          to: email,
-          subject: 'Reset your 86it password',
-          html: `Please <a href="${url}">click here</a> to reset your password. 
-               If you didn't request this, please ignore this email.`,
-        },
-        function (err, info) {
-          if (err) {
-            return next(createError(500, err));
-          }
-          res
-            .status(200)
-            .json({ message: `Reset password email sent. ${info.response}` });
-        }
+export default {
+  verifyEmail: async function (req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!(authHeader && authHeader.startsWith('Bearer '))) {
+      return next(
+        createError(401, 'Request headers are missing authorization.')
       );
     }
-  );
-}
 
-export async function resetPassword(req, res, next) {
-  const { password } = req.body;
-  if (!password) {
-    return next(createError(400, 'Request body is missing password.'));
-  }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, config.gmail.secret, async function (err, decoded) {
+      if (err) {
+        return next(createError(403, 'Failed to verify token.'));
+      }
+      const user = await User.findOne({ _id: decoded.id }).exec();
+      if (!user) {
+        return next(createError(404, 'User not found.'));
+      }
+      user.emailVerified = true;
+      await user.save();
+    });
 
-  const authHeader = req.headers.authorization;
-  if (!(authHeader && authHeader.startsWith('Bearer '))) {
-    return next(createError(401, 'Request headers are missing authorization.'));
-  }
+    return res.status(200).json('Email verified.');
+  },
 
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, config.gmail.secret, async function (err, decoded) {
-    if (err) {
-      return next(createError(403, 'Failed to verify token.'));
+  sendResetPasswordEmail: async function (req, res, next) {
+    const { email } = req.body;
+    if (!email) {
+      return next(createError(400, 'Request body is missing email.'));
     }
-    const user = await User.findOne({ _id: decoded.id }).exec();
+
+    const user = await User.findOne({ email: email }).exec();
     if (!user) {
       return next(createError(404, 'User not found.'));
     }
     if (!user.emailVerified) {
       return next(createError(403, 'Email not verified.'));
     }
-    user.password = password;
-    await user.save();
-  });
 
-  return res.status(200).json('Password reset.');
-}
-
-export async function signIn(req, res, next) {
-  const { email, password } = req.body;
-  if (!(email && password)) {
-    return next(createError(400, 'Request body is missing email or password.'));
-  }
-
-  const user = await User.findOne({ email: email }).exec();
-  if (!user) {
-    return next(createError(404, 'User not found.'));
-  }
-  if (!user.emailVerified) {
-    return next(createError(403, 'Email not verified.'));
-  }
-
-  const match = await user.comparePassword(password);
-  if (!match) {
-    return next(createError(401, 'Incorrect password.'));
-  }
-
-  jwt.sign(
-    { id: user._id },
-    config.gmail.secret,
-    { expiresIn: '1d' },
-    function (err, token) {
-      if (err) {
-        return next(createError(500, err));
+    jwt.sign(
+      { id: user._id },
+      config.gmail.secret,
+      { expiresIn: '1d' },
+      function (err, token) {
+        if (err) {
+          return next(createError(500, err));
+        }
+        const url = `http://localhost:5173/temp/reset-password?token=${token}`;
+        transporter.sendMail(
+          {
+            to: email,
+            subject: 'Reset your 86it password',
+            html: `Please <a href="${url}">click here</a> to reset your password. 
+          If you didn't request this, please ignore this email.`,
+          },
+          function (err, info) {
+            if (err) {
+              return next(createError(500, err));
+            }
+            res
+              .status(200)
+              .json({ message: `Reset password email sent. ${info.response}` });
+          }
+        );
       }
-      return res
-        .status(200)
-        .clearCookie('86it')
-        .cookie('86it', token, { httpOnly: true })
-        .json('Signed in. Cookie set.');
+    );
+  },
+
+  resetPassword: async function (req, res, next) {
+    const { password } = req.body;
+    if (!password) {
+      return next(createError(400, 'Request body is missing password.'));
     }
-  );
-}
+
+    const authHeader = req.headers.authorization;
+    if (!(authHeader && authHeader.startsWith('Bearer '))) {
+      return next(
+        createError(401, 'Request headers are missing authorization.')
+      );
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, config.gmail.secret, async function (err, decoded) {
+      if (err) {
+        return next(createError(403, 'Failed to verify token.'));
+      }
+      const user = await User.findOne({ _id: decoded.id }).exec();
+      if (!user) {
+        return next(createError(404, 'User not found.'));
+      }
+      if (!user.emailVerified) {
+        return next(createError(403, 'Email not verified.'));
+      }
+      user.password = password;
+      await user.save();
+    });
+
+    return res.status(200).json('Password reset.');
+  },
+
+  signIn: async function (req, res, next) {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+      return next(
+        createError(400, 'Request body is missing email or password.')
+      );
+    }
+
+    const user = await User.findOne({ email: email }).exec();
+    if (!user) {
+      return next(createError(404, 'User not found.'));
+    }
+    if (!user.emailVerified) {
+      return next(createError(403, 'Email not verified.'));
+    }
+
+    const match = await user.comparePassword(password);
+    if (!match) {
+      return next(createError(401, 'Incorrect password.'));
+    }
+
+    jwt.sign(
+      { id: user._id },
+      config.gmail.secret,
+      { expiresIn: '1d' },
+      function (err, token) {
+        if (err) {
+          return next(createError(500, err));
+        }
+        return res
+          .status(200)
+          .clearCookie('86it')
+          .cookie('86it', token, { httpOnly: true })
+          .json('Signed in. Cookie set.');
+      }
+    );
+  },
+};
--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -6,48 +6,50 @@ import transporter from '../utils/mail.js';
 import createError from 'http-errors';
 import jwt from 'jsonwebtoken';
 
-export async function createUser(req, res, next) {
-  const { firstName, lastName, email, password } = req.body;
-  if (!(firstName && lastName && email && password)) {
-    return next(
-      createError(
-        400,
-        'Request body is missing firstName, lastName, email, or password.'
-      )
-    );
-  }
-
-  const user = new User({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-  });
-  await user.save();
-
-  jwt.sign(
-    { id: user.id },
-    config.gmail.secret,
-    { expiresIn: '1d' },
-    function (err, token) {
-      if (err) {
-        return next(createError(500, err));
-      }
-      const url = `http://localhost:5173/temp/verification?token=${token}`;
-      transporter.sendMail(
-        {
-          to: email,
-          subject: 'Verify your 86it account',
-          html: `Please <a href="${url}">click here</a> to verify your email. 
-             If you didn't request this, please ignore this email.`,
-        },
-        function (err, info) {
-          if (err) {
-            return next(createError(500, err));
-          }
-          res.status(200).json(`Verification email sent. ${info.response}`);
-        }
+export default {
+  createUser: async function (req, res, next) {
+    const { firstName, lastName, email, password } = req.body;
+    if (!(firstName && lastName && email && password)) {
+      return next(
+        createError(
+          400,
+          'Request body is missing firstName, lastName, email, or password.'
+        )
       );
     }
-  );
-}
+
+    const user = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    });
+    await user.save();
+
+    jwt.sign(
+      { id: user.id },
+      config.gmail.secret,
+      { expiresIn: '1d' },
+      function (err, token) {
+        if (err) {
+          return next(createError(500, err));
+        }
+        const url = `http://localhost:5173/temp/verification?token=${token}`;
+        transporter.sendMail(
+          {
+            to: email,
+            subject: 'Verify your 86it account',
+            html: `Please <a href="${url}">click here</a> to verify your email. 
+             If you didn't request this, please ignore this email.`,
+          },
+          function (err, info) {
+            if (err) {
+              return next(createError(500, err));
+            }
+            res.status(200).json(`Verification email sent. ${info.response}`);
+          }
+        );
+      }
+    );
+  },
+};
