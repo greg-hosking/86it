df249345684db779d92d6cbd1183cb94b4c58b17
Greg Hosking <hoskinggregory@gmail.com>
2023-04-13 17:41:06 -0400

    Add error messages for auth middleware

--- api/middleware/auth.middleware.js
+++ api/middleware/auth.middleware.js
@@ -6,12 +6,12 @@ import jwt from 'jsonwebtoken';
 export default async function (req, res, next) {
   const token = req.cookies['86it'];
   if (!token) {
-    return next(createError(401));
+    return next(createError(401, 'Request cookies are missing token.'));
   }
 
   jwt.verify(token, config.gmail.secret, function (err, decoded) {
     if (err) {
-      return next(createError(403));
+      return next(createError(403, 'Failed to verify token.'));
     }
     req.user = { id: decoded.id };
     next();
