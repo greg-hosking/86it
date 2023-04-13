b258f88085dbc2443c8e975c41b3176c174c8553
Greg Hosking <hoskinggregory@gmail.com>
2023-04-12 20:54:39 -0400

    Implement forgot and reset password end points

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import mongodb from 'mongodb';
 
 import User from '../models/user.js';
 
 import config from '../utils/env.js';
-import withDB from '../utils/db.js';
 
-import bcrypt from 'bcrypt';
 import createError from 'http-errors';
 import jwt from 'jsonwebtoken';
 import nodemailer from 'nodemailer';
@@ -28,27 +25,105 @@ router.get('/verification', async function (req, res, next) {
   // Validate request headers.
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return next(createError(401));
+    return next(createError(401, 'Request headers are missing authorization.'));
   }
 
   // Verify token.
   const token = authHeader.split(' ')[1];
   jwt.verify(token, config.gmail.secret, async function (err, decoded) {
     if (err) {
-      return next(createError(403));
+      return next(createError(403, 'Failed to verify token.'));
     }
 
     // Once token verified, update emailVerified in user.
-    try {
-      await User.findByIdAndUpdate(
-        decoded.id,
-        { emailVerified: true },
-        { new: true }
-      );
-      return res.status(200).json('Email verified.');
-    } catch (err) {
-      return next(createError(500, err));
+    const user = await User.findOne({ _id: decoded.id }).exec();
+    if (!user) {
+      return next(createError(404, 'User not found.'));
     }
+
+    user.emailVerified = true;
+    await user.save();
+
+    return res.status(200).json('Email verified.');
+  });
+});
+
+/**
+ *
+ */
+router.post('/forgot-password', async function (req, res, next) {
+  const { email } = req.body;
+  if (!email) {
+    return next(createError(400, 'Request body is missing email.'));
+  }
+
+  const user = await User.findOne({ email: email }).exec();
+  if (!user) {
+    return next(createError(404, 'User not found.'));
+  }
+  if (!user.emailVerified) {
+    return next(createError(403, 'Email not verified.'));
+  }
+
+  // Send recovery email asynchronously.
+  jwt.sign(
+    { id: user._id },
+    config.gmail.secret,
+    { expiresIn: '1d' },
+    function (err, token) {
+      if (err) {
+        return next(createError(500, err));
+      }
+      const url = `http://localhost:5173/temp/reset-password?token=${token}`;
+
+      transporter.sendMail({
+        to: email,
+        subject: 'Recover your 86it password',
+        html: `Please <a href="${url}">click here</a> to recover your password. 
+               If you didn't request this, please ignore this email.`,
+      });
+    }
+  );
+
+  res.status(200).json({ message: 'Recovery email sent.' });
+});
+
+/**
+ *
+ */
+router.post('/reset-password', async function (req, res, next) {
+  // Validate request body.
+  const { password } = req.body;
+  if (!password) {
+    return next(createError(400, 'Request body is missing password.'));
+  }
+
+  // Validate request headers.
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return next(createError(401, 'Request headers are missing authorization.'));
+  }
+
+  // Verify token.
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, config.gmail.secret, async function (err, decoded) {
+    if (err) {
+      return next(createError(403, 'Failed to verify token.'));
+    }
+
+    // Once token verified, update user password.
+    const user = await User.findOne({ _id: decoded.id }).exec();
+    if (!user) {
+      return next(createError(404, 'User not found.'));
+    }
+    if (!user.emailVerified) {
+      return next(createError(403, 'Email not verified.'));
+    }
+
+    user.password = password;
+    await user.save();
+
+    return res.status(200).json('Password reset.');
   });
 });
 
@@ -61,85 +136,34 @@ router.post('/', async function (req, res, next) {
     return next(createError(400, 'Request body is missing email or password.'));
   }
 
-  User.findOne({ email: email }, async function (err, user) {
-    if (err) {
-      return next(createError(500, err));
-    }
-    if (!user) {
-      return next(createError(404));
-    }
-    if (!user.emailVerified) {
-      return next(createError(403));
-    }
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return next(createError(401));
-    }
-    jwt.sign(
-      {
-        id: user._id,
-      },
-      config.gmail.secret,
-
-      {
-        expiresIn: '1d',
-      },
-      function (err, token) {
-        if (err) {
-          return next(createError(500, err));
-        }
-        return res
-          .status(200)
-          .clearCookie('86itCookie')
-          .cookie('86itCookie', token, { httpOnly: true })
-          .send();
-      }
-    );
-  });
-});
-
-/**
- *
- */
-router.post('/recovery', async function (req, res, next) {
-  const email = req.body.email;
-  if (!email) {
-    return next(createError(400, 'Request body is missing email.'));
+  const user = await User.findOne({ email: email }).exec();
+  if (!user) {
+    return next(createError(404, 'User not found.'));
+  }
+  if (!user.emailVerified) {
+    return next(createError(403, 'Email not verified.'));
   }
 
-  await withDB(async function (db) {
-    const users = db.collection('users');
-    const user = users.findOne({ email: email });
-    if (!user) {
-      return next(createError(404));
-    }
+  const match = await user.comparePassword(password);
+  if (!match) {
+    return next(createError(401, 'Incorrect password.'));
+  }
 
-    // Send recovery email asynchronously.
-    jwt.sign(
-      {
-        id: user._id,
-      },
-      config.gmail.secret,
-      {
-        expiresIn: '1d',
-      },
-      function (err, token) {
-        if (err) {
-          return next(createError(500, err));
-        }
-        const url = `http://localhost:5173/temp/reset-password?token=${token}`;
-
-        transporter.sendMail({
-          to: email,
-          subject: 'Recover your 86it password',
-          html: `Please <a href="${url}">click here</a> to recover your password. 
-                 If you didn't request this, please ignore this email.`,
-        });
+  jwt.sign(
+    { id: user._id },
+    config.gmail.secret,
+    { expiresIn: '1d' },
+    function (err, token) {
+      if (err) {
+        return next(createError(500, err));
       }
-    );
-  }, res);
-
-  res.status(200).json({ message: 'Recovery email sent.' });
+      return res
+        .status(200)
+        .clearCookie('86itCookie')
+        .cookie('86itCookie', token, { httpOnly: true })
+        .send();
+    }
+  );
 });
 
 export default router;
