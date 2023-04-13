bd3135f8a61aacb73f5ad074e06d186d4f1daed3
Greg Hosking <hoskinggregory@gmail.com>
2023-04-13 13:45:48 -0400

    Add AWS credentials for SDK

--- api/utils/env.js
+++ api/utils/env.js
@@ -10,6 +10,12 @@ export default {
   db: {
     uri: process.env.MONGODB_URI,
   },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    bucket: process.env.AWS_BUCKET,
+  },
   gmail: {
     host: 'smtp.gmail.com',
     user: process.env.GMAIL_USER,
