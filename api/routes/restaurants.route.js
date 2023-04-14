e6333374e96ea5ed1c98de6173cce1bfccef38bb
Greg Hosking <hoskinggregory@gmail.com>
2023-04-14 07:43:01 -0400

    Implement basic restaurant and invite handlers

--- api/routes/restaurants.route.js
+++ api/routes/restaurants.route.js
@@ -1,10 +1,58 @@
 import express from 'express';
 
+import restaurantsController from '../controllers/restaurants.controller.js';
+
+import auth from '../middleware/auth.middleware.js';
+
 import upload from '../utils/upload.js';
 
 const router = express.Router();
 
-// TODO: Add routes for restaurants...
+router.get('/', auth.isAuthenticated, restaurantsController.getRestaurants);
+
+router.get(
+  '/:restaurantId',
+  auth.isAuthenticated,
+  restaurantsController.getRestaurantById
+);
+
+router.post('/', auth.isAuthenticated, restaurantsController.createRestaurant);
+
+router.put(
+  '/:restaurantId',
+  auth.isAuthenticated,
+  auth.isRestaurantManager,
+  restaurantsController.updateRestaurant
+);
+
+router.put(
+  '/:restaurantId/image',
+  auth.isAuthenticated,
+  auth.isRestaurantManager,
+  upload.single('image'),
+  restaurantsController.updateRestaurantImage
+);
+
+router.post(
+  '/:restaurantId/users',
+  auth.isAuthenticated,
+  auth.isRestaurantManager,
+  restaurantsController.inviteUserToRestaurant
+);
+
+router.post(
+  '/:restaurantId/users/:userId/accept',
+  auth.isAuthenticated,
+  auth.isRequestedUser,
+  restaurantsController.acceptInvitation
+);
+
+router.post(
+  '/:restaurantId/users/:userId/reject',
+  auth.isAuthenticated,
+  auth.isRequestedUser,
+  restaurantsController.rejectInvitation
+);
 
 // Example route for uploading an image
 router.post(
