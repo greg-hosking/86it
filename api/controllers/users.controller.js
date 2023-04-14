ac2a3957b7a6edb2d733e325e509b224700a61e2
Greg Hosking <hoskinggregory@gmail.com>
2023-04-13 23:48:16 -0400

    Using sign up as middleware was a mistake

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -9,7 +9,7 @@ import jwt from 'jsonwebtoken';
 
 export default {
   createUser: async function (req, res, next) {
-    const { name, address, firstName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
     if (!(firstName && lastName && email && password)) {
       return next(
         createError(
@@ -19,62 +19,19 @@ export default {
       );
     }
 
-    let user = new User({
+    const user = new User({
       firstName: firstName,
       lastName: lastName,
       email: email,
       password: password,
     });
-    try {
-      // If the request headers contain a token, this request is being made to
-      // create a user that is accepting an email invitation for a role at a restaurant.
-      // In this case, the user should be automatically verified and updated.
-      const authHeader = req.headers.authorization;
-      if (authHeader && authHeader.startsWith('Bearer ')) {
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, config.gmail.secret, async function (err, decoded) {
-          if (err) {
-            return next(createError(403, 'Failed to verify token.'));
-          }
-          user.verified = true;
-          user.restaurants = [
-            {
-              restaurantId: decoded.restaurantId,
-              role: 'manager',
-            },
-          ];
-          let restaurant = await Restaurant.findOne({
-            _id: decoded.restaurantId,
-          }).exec();
-          if (!restaurant) {
-            return next(createError(404, 'Restaurant not found.'));
-          }
-          restaurant.users.push({
-            userId: user._id,
-            role: 'manager',
-          });
-          await user.save();
-          await restaurant.save();
 
-          return res.status(200).json('User created. Invitation accepted.');
-        });
-      }
+    try {
       await user.save();
     } catch (error) {
-      // Duplicate key error
       if (error.code === 11000) {
-        // If the user already exists but the name and address fields are present,
-        // this function is being used as middleware to create a restaurant.
-        // Then if the user is verified, ignore the duplicate key error and continue.
-        if (name && address) {
-          const existingUser = await User.findOne({ email: email }).exec();
-          if (!existingUser.verified) {
-            return next(createError(403, 'Email not verified.'));
-          }
-          req.body.owner = { _id: existingUser._id };
-          return next();
-        }
-        return next(createError(409, 'Email already exists.'));
+        // Duplicate key error
+        return next(createError(409, 'User already exists with that email.'));
       }
       return next(createError(500, error));
     }
@@ -98,16 +55,61 @@ export default {
             if (err) {
               return next(createError(500, err));
             }
-            // If the name and address fields are present, this function is being
-            // used as middleware to create a restaurant. Call next() to continue.
-            if (name && address) {
-              req.body.owner = { _id: user._id };
-              return next();
-            }
             res.status(200).json(`Verification email sent.`);
           }
         );
       }
     );
   },
+
+  getUserRestaurants: async function (req, res, next) {
+    const { userId } = req.params;
+    const user = await User.findOne({ _id: userId }).exec();
+    if (!user) {
+      return next(createError(404, 'User not found.'));
+    }
+    const restaurants = user.restaurants.map(async (restaurantId) => {
+      return await Restaurant.findOne({ _id: restaurantId }).exec();
+    });
+    res.status(200).json(restaurants);
+  },
 };
+
+// This snippet of code aims to create a user that is accepting an email invitation
+// for a role at a restaurant. I am considering a different approach that would
+// require the user to create an account and then accept the invitation from the
+// invitation inbox or something like that.
+
+// // If the request headers contain a token, this request is being made to
+// // create a user that is accepting an email invitation for a role at a restaurant.
+// // In this case, the user should be automatically verified and updated.
+// const authHeader = req.headers.authorization;
+// if (authHeader && authHeader.startsWith('Bearer ')) {
+//   const token = authHeader.split(' ')[1];
+//   jwt.verify(token, config.gmail.secret, async function (err, decoded) {
+//     if (err) {
+//       return next(createError(403, 'Failed to verify token.'));
+//     }
+//     user.verified = true;
+//     user.restaurants = [
+//       {
+//         restaurantId: decoded.restaurantId,
+//         role: 'manager',
+//       },
+//     ];
+//     let restaurant = await Restaurant.findOne({
+//       _id: decoded.restaurantId,
+//     }).exec();
+//     if (!restaurant) {
+//       return next(createError(404, 'Restaurant not found.'));
+//     }
+//     restaurant.users.push({
+//       userId: user._id,
+//       role: 'manager',
+//     });
+//     await user.save();
+//     await restaurant.save();
+
+//     return res.status(200).json('User created. Invitation accepted.');
+//   });
+// }
