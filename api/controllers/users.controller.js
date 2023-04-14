7650d48d00a7b2d0b2f4d8deef45e599f1486738
Greg Hosking <hoskinggregory@gmail.com>
2023-04-13 20:53:29 -0400

    Refactor createUser to serve as middleware for createRestaurant

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import Restaurant from '../models/restaurant.model.js';
 
 import config from '../utils/env.js';
 import transporter from '../utils/mail.js';
@@ -8,7 +9,7 @@ import jwt from 'jsonwebtoken';
 
 export default {
   createUser: async function (req, res, next) {
-    const { firstName, lastName, email, password } = req.body;
+    const { name, address, firstName, lastName, email, password } = req.body;
     if (!(firstName && lastName && email && password)) {
       return next(
         createError(
@@ -18,16 +19,68 @@ export default {
       );
     }
 
-    const user = new User({
+    let user = new User({
       firstName: firstName,
       lastName: lastName,
       email: email,
       password: password,
     });
-    await user.save();
+    try {
+      // If the request headers contain a token, this request is being made to
+      // create a user that is accepting an email invitation for a role at a restaurant.
+      // In this case, the user should be automatically verified and updated.
+      const authHeader = req.headers.authorization;
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, config.gmail.secret, async function (err, decoded) {
+          if (err) {
+            return next(createError(403, 'Failed to verify token.'));
+          }
+          user.verified = true;
+          user.restaurants = [
+            {
+              restaurantId: decoded.restaurantId,
+              role: 'manager',
+            },
+          ];
+          let restaurant = await Restaurant.findOne({
+            _id: decoded.restaurantId,
+          }).exec();
+          if (!restaurant) {
+            return next(createError(404, 'Restaurant not found.'));
+          }
+          restaurant.users.push({
+            userId: user._id,
+            role: 'manager',
+          });
+          await user.save();
+          await restaurant.save();
+
+          return res.status(200).json('User created. Invitation accepted.');
+        });
+      }
+      await user.save();
+    } catch (error) {
+      // Duplicate key error
+      if (error.code === 11000) {
+        // If the user already exists but the name and address fields are present,
+        // this function is being used as middleware to create a restaurant.
+        // Then if the user is verified, ignore the duplicate key error and continue.
+        if (name && address) {
+          const existingUser = await User.findOne({ email: email }).exec();
+          if (!existingUser.verified) {
+            return next(createError(403, 'Email not verified.'));
+          }
+          req.body.owner = { _id: existingUser._id };
+          return next();
+        }
+        return next(createError(409, 'Email already exists.'));
+      }
+      return next(createError(500, error));
+    }
 
     jwt.sign(
-      { id: user.id },
+      { id: user._id },
       config.gmail.secret,
       { expiresIn: '1d' },
       function (err, token) {
@@ -39,14 +92,19 @@ export default {
           {
             to: email,
             subject: 'Verify your 86it account',
-            html: `Please <a href="${url}">click here</a> to verify your email. 
-             If you didn't request this, please ignore this email.`,
+            html: `Please <a href="${url}">click here</a> to verify your email.`,
           },
-          function (err, info) {
+          async function (err, info) {
             if (err) {
               return next(createError(500, err));
             }
-            res.status(200).json(`Verification email sent. ${info.response}`);
+            // If the name and address fields are present, this function is being
+            // used as middleware to create a restaurant. Call next() to continue.
+            if (name && address) {
+              req.body.owner = { _id: user._id };
+              return next();
+            }
+            res.status(200).json(`Verification email sent.`);
           }
         );
       }
