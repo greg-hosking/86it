9bf7e1a49ed0beaa49b857bfa81aeda6ea3745d1
Greg Hosking <hoskinggregory@gmail.com>
2023-04-25 23:18:38 -0400

    Refactor with auth context and error component

---
+++ client/src/pages/auth/SignIn.jsx
@@ -0,0 +1,137 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import AuthContext from '../../contexts/AuthContext.jsx';
+import Error from '../../components/Error.jsx';
+
+export default function SignIn() {
+  const { authenticatedUser, signIn } = useContext(AuthContext.AuthContext);
+  const navigate = useNavigate();
+
+  const token = new URLSearchParams(window.location.search).get('token');
+  const [verified, setVerified] = useState(false);
+  const shouldVerify = useRef(true);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  // Redirect to /me/restaurants if user is already signed in
+  useEffect(() => {
+    if (authenticatedUser) {
+      navigate('/me/restaurants');
+    }
+
+    async function verify() {
+      setLoading(true);
+      const response = await fetch('/api/users/me/verified', {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      switch (response.status) {
+        case 200:
+          setVerified(true);
+          break;
+        case 401:
+        case 403:
+          setError('Invalid token');
+          break;
+        default:
+          setError('An unknown error occurred');
+      }
+      setLoading(false);
+    }
+    if (token && shouldVerify.current) {
+      verify();
+      shouldVerify.current = false;
+    }
+  }, [authenticatedUser]);
+
+  return (
+    <div className='content-container content-container-sm'>
+      {token && !verified ? (
+        <>{loading ? <p>Verifying...</p> : <Error error={error} />}</>
+      ) : (
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setLoading(true);
+            const status = await signIn(email, password);
+            switch (status) {
+              case 200:
+                navigate('/me/restaurants');
+                break;
+              case 401:
+                setError('Incorrect email or password');
+                break;
+              case 403:
+                setError('Account not verified');
+                break;
+              case 404:
+                setError('Account not found');
+                break;
+              default:
+                setError('An unknown error occurred');
+            }
+            setLoading(false);
+          }}
+        >
+          <h1>Sign In</h1>
+          {error && (
+            <Error
+              error={error}
+              dismiss={() => {
+                setError('');
+              }}
+            />
+          )}
+          <div>
+            <label htmlFor='email-input'>Email</label>
+            <input
+              type='email'
+              name='email-input'
+              id='email-input'
+              required
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setError('');
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor='password-input'>Password</label>
+            <input
+              type='password'
+              name='password-input'
+              id='password-input'
+              required
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError('');
+              }}
+            />
+            <a href='/forgot-password' id='forgot-password-link'>
+              Forgot password?
+            </a>
+          </div>
+          <input
+            type='submit'
+            value={loading ? 'Loading' : 'Sign In'}
+            disabled={loading}
+          />
+          <p>
+            Don't have an account? <a href='/sign-up'>Sign up.</a>
+          </p>
+        </form>
+      )}
+    </div>
+  );
+}
---
+++ client/src/pages/auth/SignUp.jsx
@@ -0,0 +1,174 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import AuthContext from '../../contexts/AuthContext.jsx';
+import Error from '../../components/Error.jsx';
+
+export default function SignUp() {
+  const { authenticatedUser } = useContext(AuthContext.AuthContext);
+  const navigate = useNavigate();
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  // Redirect to /me/restaurants if user is already signed in
+  useEffect(() => {
+    if (authenticatedUser) {
+      navigate('/me/restaurants');
+    }
+  }, [authenticatedUser]);
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    setLoading(true);
+    if (password1 !== password2) {
+      setPassword1('');
+      setPassword2('');
+      setError('Passwords do not match');
+      setLoading(false);
+      return;
+    }
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password1,
+      }),
+    });
+    switch (response.status) {
+      case 201:
+        setSuccess(true);
+        break;
+      case 409:
+        setError('An account with that email already exists');
+        break;
+      default:
+        setError('An unknown error occurred');
+    }
+    setLoading(false);
+  }
+
+  return (
+    <div className='content-container content-container-sm'>
+      <form
+        onSubmit={(e) => {
+          handleSignUp(e);
+        }}
+      >
+        {success ? (
+          <>
+            <p>Account successfully created!</p>
+            <p>
+              Verification email sent to{' '}
+              <b style={{ color: 'var(--text-accent)' }}>{email}</b>
+            </p>
+          </>
+        ) : (
+          <>
+            <h1>Sign Up</h1>
+            {error && (
+              <Error
+                error={error}
+                dismiss={() => {
+                  setError('');
+                }}
+              />
+            )}
+            <div>
+              <label htmlFor='first-name-input'>First Name</label>
+              <input
+                type='text'
+                name='first-name-input'
+                id='first-name-input'
+                required={true}
+                value={firstName}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                  setError('');
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='last-name-input'>Last Name</label>
+              <input
+                type='text'
+                name='last-name-input'
+                id='last-name-input'
+                required={true}
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                  setError('');
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='email-input'>Email</label>
+              <input
+                type='email'
+                name='email-input'
+                id='email-input'
+                required={true}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setError('');
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='password1-input'>Password</label>
+              <input
+                type='password'
+                name='password1-input'
+                id='password1-input'
+                required={true}
+                value={password1}
+                onChange={(e) => {
+                  setPassword1(e.target.value);
+                  setPassword2('');
+                  setError('');
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='password2-input'>Confirm Password</label>
+              <input
+                type='password'
+                name='password2-input'
+                id='password2-input'
+                required={true}
+                value={password2}
+                onChange={(e) => {
+                  setPassword2(e.target.value);
+                  setError('');
+                }}
+              />
+            </div>
+            <input
+              type='submit'
+              value={loading ? 'Loading' : 'Sign Up'}
+              disabled={loading}
+            />
+            <p>
+              Already have an account? <a href='/sign-in'>Sign in.</a>
+            </p>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
