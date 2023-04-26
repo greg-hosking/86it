4c15659585f649fa967ff30f6eb0f05dcebf9bf7
Greg Hosking <hoskinggregory@gmail.com>
2023-04-25 23:04:54 -0400

    Change client port

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    open: true,
+    port: 3000,
     proxy: {
       '/api': 'http://localhost:3001/',
     },
