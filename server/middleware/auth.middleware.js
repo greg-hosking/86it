97f7db929e2a93f2ef9ec06335bff9ebee2db6db
Greg Hosking <hoskinggregory@gmail.com>
2023-04-25 23:11:08 -0400

    Rename api folder to server

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,78 @@
+import Restaurant from '../models/restaurant.model.js';
+
+import config from '../utils/env.js';
+
+import createError from 'http-errors';
+import jwt from 'jsonwebtoken';
+
+export default {
+  isAuthenticated: async function (req, res, next) {
+    const token = req.cookies['86it'];
+    if (!token) {
+      return next(createError(401, 'Request cookies are missing token.'));
+    }
+
+    jwt.verify(token, config.server.secret, function (err, decoded) {
+      if (err) {
+        return next(createError(403, 'Failed to verify token.'));
+      }
+      // Once verified, add the authenticated user to the request object so
+      // that it can be used in the controller function that is called next.
+      req.authenticatedUser = { _id: decoded._id };
+      next();
+    });
+  },
+
+  isRequestedUser: async function (req, res, next) {
+    const { _id } = req.authenticatedUser;
+    const { userId } = req.params;
+
+    if (userId !== _id) {
+      return next(createError(403, 'Unauthorized.'));
+    }
+
+    next();
+  },
+
+  isRestaurantOwner: async function (req, res, next) {
+    const { restaurantId } = req.params;
+    const { _id } = req.authenticatedUser;
+
+    if (!restaurantId) {
+      return next(createError(400, 'Request params is missing restaurantId.'));
+    }
+
+    const restaurant = await Restaurant.findOne({ _id: restaurantId }).exec();
+    if (!restaurant) {
+      return next(createError(404, 'Restaurant not found.'));
+    }
+
+    const user = restaurant.users.find((user) => user.userId === _id);
+    if (!user || user.role !== 'owner') {
+      return next(createError(403, 'Unauthorized.'));
+    }
+
+    next();
+  },
+
+  isRestaurantManager: async function (req, res, next) {
+    const { restaurantId } = req.params;
+    const { _id } = req.authenticatedUser;
+
+    if (!restaurantId) {
+      return next(createError(400, 'Request params is missing restaurantId.'));
+    }
+
+    const restaurant = await Restaurant.findOne({ _id: restaurantId }).exec();
+    if (!restaurant) {
+      return next(createError(404, 'Restaurant not found.'));
+    }
+
+    const user = restaurant.users.find((user) => user.userId == _id);
+    if (!user || (user.role !== 'manager' && user.role !== 'owner')) {
+      return next(createError(403, 'Unauthorized.'));
+    }
+
+    next();
+  },
+};
---
+++ server/models/user.model.js
@@ -0,0 +1,85 @@
+import mongoose from 'mongoose';
+
+import bcrypt from 'bcrypt';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'First name is required.'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required.'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: [true, 'Email already exists.'],
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.'],
+    },
+    image: {
+      type: String,
+      default: 'https://86it.s3.amazonaws.com/icons/user.svg',
+    },
+    restaurants: {
+      type: [
+        {
+          restaurantId: {
+            type: mongoose.ObjectId,
+            ref: 'Restaurant',
+            required: [true, 'Restaurant ID is required.'],
+          },
+          role: {
+            type: String,
+            enum: ['owner', 'manager', 'pending'],
+            required: [true, 'Role is required.'],
+          },
+        },
+      ],
+      default: [],
+    },
+    favoriteItems: [
+      {
+        type: mongoose.ObjectId,
+        ref: 'Item',
+        default: [],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * Hash user password before saving.
+ */
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  // Only hash the password if it has been modified (or is new).
+  if (!user.isModified('password')) {
+    return next();
+  }
+  const rounds = 10;
+  const hash = await bcrypt.hash(user.password, rounds);
+
+  user.password = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model('User', userSchema);
