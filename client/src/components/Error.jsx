b7b8cff9bdf2941544f06e599ce855fb6732bc71
Greg Hosking <hoskinggregory@gmail.com>
2023-04-25 23:07:25 -0400

    Make error centered if not dismissible

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 function Error({ error, dismiss }) {
   return (
     <div className='error-container'>
-      {error}
-      <a onClick={dismiss}>X</a>
+      <p
+        style={{
+          textAlign: dismiss ? 'left' : 'center',
+          width: !dismiss && '100%',
+        }}
+      >
+        {error}
+      </p>
+      {dismiss && <a onClick={dismiss}>X</a>}
     </div>
   );
 }
