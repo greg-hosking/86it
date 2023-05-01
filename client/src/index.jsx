5c900ea69f78acef3eeb9dbe6e6a569e5d8c258d
Greg Hosking <hoskinggregory@gmail.com>
2023-05-01 14:50:37 -0400

    Add restaurant context

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import AuthContext from './contexts/AuthContext.jsx';
+import RestaurantContext from './contexts/RestaurantContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContext.AuthProvider>
-      <App />
+      <RestaurantContext.RestaurantProvider>
+        <App />
+      </RestaurantContext.RestaurantProvider>
     </AuthContext.AuthProvider>
   </React.StrictMode>
 );
